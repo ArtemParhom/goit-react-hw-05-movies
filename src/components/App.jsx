1af5fefc4decf40f0a3387edf8cf71cef049a8e2
ArtemParhom <artkievukr@ukr.net>
2023-05-12 15:52:00 +0300

    add homepage json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-export const App = () => {
+import { Route, Routes, NavLink } from 'react-router-dom';
+
+const App = () => {
   return (
     <div
       style={{
@@ -7,10 +9,15 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <nav></nav>
+      {/* <Routes>
+        <Route></Route>
+      </Routes> */}
     </div>
   );
 };
+export default App;
